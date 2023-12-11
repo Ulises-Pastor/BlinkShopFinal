321f205cf6378b6a7e5dc488f934cc698fb2a7b4
Ulises-Pastor <pamu010914@gs.utm.mx>
2023-12-11 06:47:25 -0600

    final Commit

--- server/src/build/keys.js
+++ server/src/build/keys.js
@@ -5,7 +5,7 @@ exports.default = {
         host: 'database-1.cdpckufoj9jt.us-east-2.rds.amazonaws.com',
         user: 'admin',
         password: 'noobMaster69',
-        database: 'blink',
-        ssl: true
+        ssl: true,
+        database: 'blink'
     }
 };
