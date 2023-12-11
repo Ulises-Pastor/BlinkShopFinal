a218416b1f5c5b9f86ba5aaa70e500ba1e35868f
Ulises-Pastor <pamu010914@gs.utm.mx>
2023-12-11 06:29:55 -0600

    second commit

--- server/src/build/keys.js
+++ server/src/build/keys.js
@@ -2,10 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'Blink',
+        host: 'database-1.cdpckufoj9jt.us-east-2.rds.amazonaws.com',
+        user: 'admin',
+        password: 'noobMaster69',
+        database: 'blink',
         ssl: true
     }
 };
